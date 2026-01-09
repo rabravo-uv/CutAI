993df7e28b212f159b6806cb47fd46b155fc6c65
Rafael Bravo Rz <136104325+rabravo-uv@users.noreply.github.com>
2026-01-09 14:54:30 -0600

    NumerariDicembre 2025

---
+++ js/numeralia.js
@@ -0,0 +1,29 @@
+
+/* global $ */
+var tr=[278,277,57227,0]; 
+var ai=[641,578,7,34];
+var pdp=[46,42,86,0];
+var ct=[44,155,29,37,51,16,32];
+var tt=[626];
+$(document).ready(function(){
+        $("#ai0").text(ai[0]).number( true, 0 );
+        $("#ai1").text(ai[1]).number( true, 0 );
+        $("#ai2").text(ai[2]).number( true, 0 );
+        $("#ai3").text(ai[3]).number( true, 0 );
+        $("#pdp0").text(pdp[0]).number( true, 0 );
+        $("#pdp1").text(pdp[1]).number( true, 0 );
+        $("#pdp2").text(pdp[2]).number( true, 0 );
+        $("#pdp3").text(pdp[3]).number( true, 0 );
+        $("#tr0").text(tr[0]).number( true, 0 );
+        $("#tr1").text(tr[1]).number( true, 0 );
+        $("#tr2").text(tr[2]).number( true, 0 );
+        $("#tr3").text(tr[3]).number( true, 0 );
+        $("#ct0").text(ct[0]).number( true, 0 );
+        $("#ct1").text(ct[1]).number( true, 0 );
+        $("#ct2").text(ct[2]).number( true, 0 );
+        $("#ct3").text(ct[3]).number( true, 0 );
+        $("#ct4").text(ct[4]).number( true, 0 );
+        $("#ct5").text(ct[5]).number( true, 0 );
+        $("#ct6").text(ct[6]).number( true, 0 );        
+        $("#tt0").text(tt[0]).number( true, 0 );
+})
