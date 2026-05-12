020d8e6f0d89a9c1874e35fae409c24bab384d94
Rafael Bravo Rz <136104325+rabravo-uv@users.noreply.github.com>
2026-05-12 11:08:11 -0600

    cambi circular 2

--- js/circular.js
+++ js/circular.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     $("#m100, #m101, #m102, #m103, #m104, #m105").click(function(){
-    $("#collapseComunicado, #collapseCircular, #collapseDestruccion").collapse('hide');
+    $("#collapseComunicado, #collapseCircular, #collapseDestruccion").collapse('hide')});
     $("#m100, #m101, #m102, #m103, #m104, #m105").hide();
     $("#m000").show();
   });
-  })
