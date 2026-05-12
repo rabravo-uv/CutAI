a28382a6d760c625d6a48d12d358d991b5a007f6
Rafael Bravo Rz <136104325+rabravo-uv@users.noreply.github.com>
2026-05-12 10:14:50 -0600

    despley

--- js/circular.js
+++ js/circular.js
@@ -2,22 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     $("#m100, #m101, #m102, #m103, #m104, #m105").click(function(){
     $("#collapseComunicado, #collapseCircular, #collapseDestruccion").collapse('hide');
     $("#m100, #m101, #m102, #m103, #m104, #m105").hide();
-    $("#m0").show();
+    $("#m000").show();
   });
   $("#m1").click(function(){
-        $("#m0").hide(); 
+        $("#m000").hide(); 
         $("#collapseComunicado").collapse('hide');
         $("#collapseDestruccion").collapse('hide');
         $("#m100, #m101").show();
   });  
   $("#m2").click(function(){
-        $("#m0").hide(); 
+        $("#m000").hide(); 
         $("#collapseCircular").collapse('hide');
         $("#collapseDestruccion").collapse('hide');
         $("#m102, #m103").show();
   });
   $("#m3").click(function(){
-        $("#m0").hide(); 
+        $("#m000").hide(); 
         $("#collapseCircular").collapse('hide');   
         $("#collapseComunicado").collapse('hide');   
         $("#m104, #m105").show();
