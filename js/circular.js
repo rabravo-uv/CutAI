11e79825394cdb4d58bcc98f0dabbad668c14edf
Rafael Bravo Rz <136104325+rabravo-uv@users.noreply.github.com>
2026-05-12 11:00:03 -0600

    combi circular.js

--- js/circular.js
+++ js/circular.js
@@ -4,22 +4,4 @@ document.addEventListener('DOMContentLoaded', function() {
     $("#m100, #m101, #m102, #m103, #m104, #m105").hide();
     $("#m000").show();
   });
-  $("#m1").click(function(){
-        $("#m000").hide(); 
-        $("#collapseComunicado").collapse('hide');
-        $("#collapseDestruccion").collapse('hide');
-        $("#m100, #m101").show();
-  });  
-  $("#m2").click(function(){
-        $("#m000").hide(); 
-        $("#collapseCircular").collapse('hide');
-        $("#collapseDestruccion").collapse('hide');
-        $("#m102, #m103").show();
-  });
-  $("#m3").click(function(){
-        $("#m000").hide(); 
-        $("#collapseCircular").collapse('hide');   
-        $("#collapseComunicado").collapse('hide');   
-        $("#m104, #m105").show();
-  });
-})
+  })
